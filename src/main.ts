a44527506fbdbe238fed038e0e5b7744a6d3fc60
Sagynysh <sagynyshkenel@gmail.com>
2024-07-04 19:14:34 +0500

    adding swagger libraries

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AuthenticationGuardGuard } from './guard/auth/authentication-guard/authentication-guard.guard';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }))
   app.useGlobalGuards(new AuthenticationGuardGuard());
+
+  const config = new DocumentBuilder()
+    .setTitle('todo list')
+    .setDescription('The todolist API description')
+    .setVersion('1.0')
+    .addTag('user')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
